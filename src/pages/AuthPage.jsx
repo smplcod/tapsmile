1f98fcd2614b754b53c7b9b5f23bbc0c0e1bdfb7
smplcod <mplcod2021@gmail.com>
2023-10-26 03:47:23 +0600

    AuthPage.jsx refactoring

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -9,9 +8,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { auth, signInWithGoogle } from "../helpers/FirebaseConfig";
-
 import UserGreeting from "../components/UserGreeting";
-
 import "../components/GoogleButton.class.css";
 import GoogleButton from "../components/GoogleButton";
 
@@ -26,22 +23,21 @@ function AuthPage() {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [profileImage, setProfileImage] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       setIsLoading(false);
     });
-  }, []);
 
-  useEffect(() => {
     const storedImage = localStorage.getItem("image");
     if (storedImage) {
       setProfileImage(storedImage);
     }
   }, [user]);
 
-  const register = async () => {
+  const handleRegister = async () => {
     try {
       await createUserWithEmailAndPassword(
         auth,
@@ -49,73 +45,74 @@ function AuthPage() {
         registerPassword
       );
       navigate("/");
-    } catch (error) {
-      console.log(error.message);
+    } catch (err) {
+      setError(t("errorRegistration") + err.message);
     }
   };
 
-  const login = async () => {
+  const handleLogin = async () => {
     try {
       await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
       navigate("/");
-    } catch (error) {
-      console.log(error.message);
+    } catch (err) {
+      setError(t("errorLogin") + err.message);
     }
   };
 
-  const logout = async () => {
-    await signOut(auth);
-    localStorage.removeItem("name");
-    localStorage.removeItem("email");
-    localStorage.removeItem("image");
-    setProfileImage(null);
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      localStorage.removeItem("name");
+      localStorage.removeItem("email");
+      localStorage.removeItem("image");
+      setProfileImage(null);
+    } catch (err) {
+      setError(t("errorLogout") + err.message);
+    }
   };
 
   return (
     <div className="App">
-      {!isLoading ? (
-        !user ? (
-          <>
-            <div>
-              <input
-                placeholder={t("email")}
-                onChange={(e) => setLoginEmail(e.target.value)}
-              />
-              <input
-                placeholder={t("password")}
-                onChange={(e) => setLoginPassword(e.target.value)}
-              />
-              <button onClick={login}>{t("signIn")}</button>
-            </div>
-            {t("or")}
-            <div>
-              <input
-                placeholder={t("email")}
-                onChange={(e) => setRegisterEmail(e.target.value)}
-              />
-              <input
-                placeholder={t("password")}
-                onChange={(e) => setRegisterPassword(e.target.value)}
-              />
-              <button onClick={register}>{t("signUp")}</button>
-            </div>
-            {t("or")}
-            <div>
-              <GoogleButton
-                signInWithGoogle={() => signInWithGoogle(navigate)}
-              />
-            </div>
-          </>
-        ) : (
-          <UserGreeting
-            user={user}
-            logout={logout}
-            profileImage={profileImage}
-          />
-        )
-      ) : (
+      {isLoading ? (
         <>Loading...</>
+      ) : user ? (
+        <UserGreeting
+          user={user}
+          logout={handleLogout}
+          profileImage={profileImage}
+        />
+      ) : (
+        <>
+          <div>
+            <input
+              placeholder={t("email")}
+              onChange={(e) => setLoginEmail(e.target.value)}
+            />
+            <input
+              placeholder={t("password")}
+              onChange={(e) => setLoginPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>{t("signIn")}</button>
+          </div>
+          {t("or")}
+          <div>
+            <input
+              placeholder={t("email")}
+              onChange={(e) => setRegisterEmail(e.target.value)}
+            />
+            <input
+              placeholder={t("password")}
+              onChange={(e) => setRegisterPassword(e.target.value)}
+            />
+            <button onClick={handleRegister}>{t("signUp")}</button>
+          </div>
+          {t("or")}
+          <div>
+            <GoogleButton signInWithGoogle={() => signInWithGoogle(navigate)} />
+          </div>
+        </>
       )}
+      {error && <div className="error-message">{error}</div>}
     </div>
   );
 }
